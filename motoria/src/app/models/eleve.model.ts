5fd265f7ba398537b7bac6b1fd6245cab3d16df4
IbtissamCTI <167759820+IbtissamCTI@users.noreply.github.com>
2025-03-26 01:32:43 +0100

    Delete motoria directory

--- motoria/src/app/models/eleve.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Eleve {
-    id_eleve: number;
-    nom_complet: string;
-  }
--- motoria/src/app/models/score-etud.ts
+++
@@ -1,5 +0,0 @@
-// models/scores-response.model.ts
-export interface ScoreEtud {
-    labels: string[];  // Thématiques
-    values: number[];  // Scores
-  }
