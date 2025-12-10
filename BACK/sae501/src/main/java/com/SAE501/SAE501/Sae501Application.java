50e25af277bb814efeeec4fb41ce0fa809bf0b55
ibti <167759820+IbtissamCTI@users.noreply.github.com>
2025-12-10 16:20:28 +0100

    Add files via upload

---
+++ BACK/sae501/src/main/java/com/SAE501/SAE501/Sae501Application.java
@@ -0,0 +1,13 @@
+package com.SAE501.SAE501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sae501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sae501Application.class, args);
+	}
+
+}
