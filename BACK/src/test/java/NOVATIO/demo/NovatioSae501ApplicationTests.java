8e05304340e73f9cb5a7f0767468104e9dd59067
Ibtissam Chtioui <Ibtissem.chti@hotmail.com>
2026-01-01 13:06:34 +0100

    Ajout des fichiers backend

---
+++ BACK/src/test/java/NOVATIO/demo/NovatioSae501ApplicationTests.java
@@ -0,0 +1,13 @@
+package NOVATIO.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovatioSae501ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
